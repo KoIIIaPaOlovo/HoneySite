1784a2dd118cb2686a893d790347fc22691e8230
KoIIIaPaOlovo <44922634+KoIIIaPaOlovo@users.noreply.github.com>
2020-08-21 01:37:57 +0300

    Uploading full project

---
+++ js/js.js
@@ -0,0 +1,7 @@
+
+    
+  $('.header__small--burger').on('click',function(e){
+      $('.header__menu--wrapper').toggleClass('header__menu--wrapper--active');
+      $(this).toggleClass('header__small--burger--active');
+  });
+    
